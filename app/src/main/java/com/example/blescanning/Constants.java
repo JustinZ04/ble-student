8e77567fa3fd194badbb98d1ec9cc01e21fc35da
Justin <jzabel04@knights.ucf.edu>
2018-11-29 20:47:54 -0500

    please work

--- app/src/main/java/com/example/blescanning/Constants.java
+++ app/src/main/java/com/example/blescanning/Constants.java
@@ -7,4 +7,6 @@ public class Constants
     public static final ParcelUuid UUID = ParcelUuid
         .fromString("cac426a3-344f-45c8-8819-ebcfe81e4b23");
     public static int ENABLE_BT_SUCCESS = 1;
+    public static final String URL = "https://ble-attendance.herokuapp.com";
+    public static final String registerStudent = "/api/students/createStudent/" + APIKeys.APIKey;
 }
